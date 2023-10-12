cbeb453db8a0ef552a5078c5faa06763fa3465ab
matteobonesini <130700773+matteobonesini@users.noreply.github.com>
2023-10-12 16:35:09 +0200

    EDIT - Developer Model

--- app/Models/Developer.php
+++ app/Models/Developer.php
@@ -9,4 +9,33 @@ class Developer extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'experience_year',
+        'curriculum',
+        'profile_picture',
+        'profile_description',
+        'address',
+        'phone_number'
+    ];
+
+    public function messages() {
+        return $this->belongsTo(Message::class);
+    }
+    
+    public function sponsorships() {
+        return $this->belongsToMany(Sponsorship::class);
+    }
+
+    public function reviews() {
+        return $this->belongsTo(Review::class);
+    }
+
+    public function votes() {
+        return $this->belongsToMany(Vote::class);
+    }
+
+    public function work_fiels() {
+        return $this->belongsToMany(WorkField::class);
+    }
 }
