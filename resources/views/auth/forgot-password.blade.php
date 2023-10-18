a8f9704e54c078282eb090320e0ec5a9ab974f78
matteobonesini <130700773+matteobonesini@users.noreply.github.com>
2023-10-18 10:12:22 +0200

    EDIT - guest layout

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,20 +5,30 @@
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
+    @if ($errors->any())
+        <div class="alert alert-error">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>- {{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <label for="email" :value="old('Email')">Email</label>
-            <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            {{-- <error :messages="$errors->get('email')" class="mt-2" /> --}}
+        <div class = "form-container">
+            <!-- Email Address -->
+            <div>
+                <label for="email" class="form-label"><span class = "text-red-500">*</span>Email</label>
+                <input type="email" id="email" name="email" class="form-input @error('email') is-invalid @enderror" placeholder="example@email.com" value="{{ old('email') }}" required>
+            </div>
+        </div>
+        
+        <div class="flex items-center justify-end mt-8">
+            <button class="btn-primary">Email Password Reset Link</button>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <button>
-                {{ __('Email Password Reset Link') }}
-            </button>
-        </div>
     </form>
 @endsection
--- resources/views/partials/headerGuest.blade.php
+++ resources/views/partials/headerGuest.blade.php
@@ -1,29 +1,31 @@
-<nav class="bg-[--bg-secondary] border-gray-200 dark:bg-[--dark-bg-secondary] h-[8%]">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="{{ route('login') }}" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BDeveloper</span>
-        </a>
-        <div class="flex md:order-2">
-
-            <a href="{{ route('login') }}">
-                <button class="btn-primary">
-                    Login
-                </button>
+<header>
+    <nav class="bg-[--bg-secondary] border-gray-200 dark:bg-[--dark-bg-secondary] h-[8%]">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="{{ route('login') }}" class="flex items-center">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BDeveloper</span>
             </a>
-
-            <a class="ms-5" href="{{ route('register') }}">
-                <button class="btn-primary">
-                    Register
+            <div class="flex md:order-2">
+    
+                <a href="{{ route('login') }}">
+                    <button class="btn-primary">
+                        Login
+                    </button>
+                </a>
+    
+                <a class="ms-5" href="{{ route('register') }}">
+                    <button class="btn-primary">
+                        Register
+                    </button>
+                </a>
+                
+                <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
                 </button>
-            </a>
-            
-            <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</header>
